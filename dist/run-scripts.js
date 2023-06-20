1522fbe04a01ac447f323cb92708ae46964cd780
dpilafian <dem@centerkey.com>
2023-06-20 03:21:05 -0700

    Release v1.2.0

--- dist/run-scripts.js
+++ dist/run-scripts.js
@@ -1,4 +1,4 @@
-//! run-scripts-util v1.1.1 ~~ https://github.com/center-key/run-scripts-util ~~ MIT License
+//! run-scripts-util v1.2.0 ~~ https://github.com/center-key/run-scripts-util ~~ MIT License
 
 import { spawn, spawnSync } from 'node:child_process';
 import chalk from 'chalk';
